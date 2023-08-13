072f10d9984277e5eaa6dda1bd4249d33c6b099b
phos589 <140889919+phos589@users.noreply.github.com>
2023-08-13 16:16:36 +0200

    Add files via upload

--- 02-imageWeb/scripts/image.js
+++ 02-imageWeb/scripts/image.js
@@ -3,15 +3,21 @@ const accesKey = "LMThUrmwY88_zAir5dEcgX6D-4JtcLfzicf0CxHA80Q";
 const formEl = document.querySelector('form');
 const inputEl = document.getElementById('search');
 const searchRe = document.querySelector('.search-result');
+const showMore = document.querySelector('.showButton');
 
+let page = 1;
 let inputData = '';
 
 async function searchImages(){
   inputData = inputEl.values;
-  const url = `https://api.unsplash.com/search/photo?page=1&query=${inputData}&client_id=${accesKey}`;
+  const url = `https://api.unsplash.com/search/photo?page=${page}&query=${inputData}&client_id=${accesKey}`;
   const recponse = await fetch(url);
   const data = await recponse.json();
-  const result = data.result;
+  const result = data.result
+
+  if(page === 1){
+    searchRe.innerHTML = '';
+  }
 
   result.map((result) =>{
     const imageWrapper = document.createElement('div');
@@ -28,9 +34,19 @@ async function searchImages(){
     imageWrapper.appendChild(imageLink);
     imageWrapper.appendChild(imageWrapper);
   });
+
+  page++
+  if(page > 1 ){
+    showMore.computedStyleMap.display = 'block'
+  }
 }
 
 formEl.addEventListener('submit', (event) => {
   event.preventDefault();
+  page = 1;
   searchImages();
 });
+
+showMore.addEventListener('click', () => {
+  searchImages();
+})
